e6827a62d76d4058162b9a1d92480a2f603378f2
Abdelmalik MEBARKI <abdelmalik.mebarki@viacesi.fr>
2023-12-04 11:40:02 +0100

    Création de la classe CLMapStock

---
+++ Projet_POO/CLMapStock.cpp
@@ -0,0 +1,124 @@
+#include "CLMapStock.h"
+
+System::String^ CLMapStock::Select(void)
+{
+    throw gcnew System::NotImplementedException();
+    // TODO: insérer une instruction return ici
+}
+
+System::String^ CLMapStock::Insert(void)
+{
+    throw gcnew System::NotImplementedException();
+    // TODO: insérer une instruction return ici
+}
+
+System::String^ CLMapStock::Delete(void)
+{
+    throw gcnew System::NotImplementedException();
+    // TODO: insérer une instruction return ici
+}
+
+System::String^ CLMapStock::Update(void)
+{
+    throw gcnew System::NotImplementedException();
+    // TODO: insérer une instruction return ici
+}
+
+void CLMapStock::setID_A(System::String^)
+{
+    throw gcnew System::NotImplementedException();
+}
+
+void CLMapStock::setR_A(System::String^)
+{
+    throw gcnew System::NotImplementedException();
+}
+
+void CLMapStock::setN_A(System::String^)
+{
+    throw gcnew System::NotImplementedException();
+}
+
+void CLMapStock::setQC_A(System::String^)
+{
+    throw gcnew System::NotImplementedException();
+}
+
+void CLMapStock::setQS_A(System::String^)
+{
+    throw gcnew System::NotImplementedException();
+}
+
+void CLMapStock::setHT_A(System::String^)
+{
+    throw gcnew System::NotImplementedException();
+}
+
+void CLMapStock::setRT_A(System::String^)
+{
+    throw gcnew System::NotImplementedException();
+}
+
+void CLMapStock::setTVA_A(System::String^)
+{
+    throw gcnew System::NotImplementedException();
+}
+
+void CLMapStock::setD_A(System::String^)
+{
+    throw gcnew System::NotImplementedException();
+}
+
+System::String^ CLMapStock::getID_A(void)
+{
+    throw gcnew System::NotImplementedException();
+    // TODO: insérer une instruction return ici
+}
+
+System::String^ CLMapStock::getR_A(void)
+{
+    throw gcnew System::NotImplementedException();
+    // TODO: insérer une instruction return ici
+}
+
+System::String^ CLMapStock::getN_A(void)
+{
+    throw gcnew System::NotImplementedException();
+    // TODO: insérer une instruction return ici
+}
+
+System::String^ CLMapStock::getQC_A(void)
+{
+    throw gcnew System::NotImplementedException();
+    // TODO: insérer une instruction return ici
+}
+
+System::String^ CLMapStock::getQS_A(void)
+{
+    throw gcnew System::NotImplementedException();
+    // TODO: insérer une instruction return ici
+}
+
+System::String^ CLMapStock::getHT_A(void)
+{
+    throw gcnew System::NotImplementedException();
+    // TODO: insérer une instruction return ici
+}
+
+System::String^ CLMapStock::getRT_A(void)
+{
+    throw gcnew System::NotImplementedException();
+    // TODO: insérer une instruction return ici
+}
+
+System::String^ CLMapStock::getTVA_A(void)
+{
+    throw gcnew System::NotImplementedException();
+    // TODO: insérer une instruction return ici
+}
+
+System::String^ CLMapStock::getD_A(void)
+{
+    throw gcnew System::NotImplementedException();
+    // TODO: insérer une instruction return ici
+}
