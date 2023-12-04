8f5cfad3bb171fa7ca23942ba3ab659fb3bdad21
Peio Rigaud <peio.rigaud@viacesi.fr>
2023-12-04 19:16:00 +0100

    correction classes CLMapEmployees

--- Projet_POO/CLMapEmployees.cpp
+++ Projet_POO/CLMapEmployees.cpp
@@ -1,90 +1,90 @@
 #include "CLMapEmployees.h"
 
-System::String^ CLMapEmployees::Select(void)
+System::String^ NS_Comp_Map_Employees::CLMapEmployees::Select(void)
 {
     throw gcnew System::NotImplementedException();
     // TODO: insérer une instruction return ici
 }
 
-System::String^ CLMapEmployees::Insert(void)
+System::String^ NS_Comp_Map_Employees::CLMapEmployees::Insert(void)
 {
     throw gcnew System::NotImplementedException();
     // TODO: insérer une instruction return ici
 }
 
-System::String^ CLMapEmployees::Delete(void)
+System::String^ NS_Comp_Map_Employees::CLMapEmployees::Delete(void)
 {
     throw gcnew System::NotImplementedException();
     // TODO: insérer une instruction return ici
 }
 
-System::String^ CLMapEmployees::Update(void)
+System::String^ NS_Comp_Map_Employees::CLMapEmployees::Update(void)
 {
     throw gcnew System::NotImplementedException();
     // TODO: insérer une instruction return ici
 }
 
-void CLMapEmployees::setID_E(System::String^)
+void NS_Comp_Map_Employees::CLMapEmployees::setID_E(System::String^)
 {
     throw gcnew System::NotImplementedException();
 }
 
-void CLMapEmployees::setN_E(System::String^)
+void NS_Comp_Map_Employees::CLMapEmployees::setN_E(System::String^)
 {
     throw gcnew System::NotImplementedException();
 }
 
-void CLMapEmployees::setS_E(System::String^)
+void NS_Comp_Map_Employees::CLMapEmployees::setS_E(System::String^)
 {
     throw gcnew System::NotImplementedException();
 }
 
-void CLMapEmployees::setHD_E(System::String^)
+void NS_Comp_Map_Employees::CLMapEmployees::setHD_E(System::String^)
 {
     throw gcnew System::NotImplementedException();
 }
 
-void CLMapEmployees::setN1_E(System::String^)
+void NS_Comp_Map_Employees::CLMapEmployees::setN1_E(System::String^)
 {
     throw gcnew System::NotImplementedException();
 }
 
-void CLMapEmployees::setST_E(System::String^)
+void NS_Comp_Map_Employees::CLMapEmployees::setST_E(System::String^)
 {
     throw gcnew System::NotImplementedException();
 }
 
-System::String^ CLMapEmployees::getID_E(void)
-{
-    throw gcnew System::NotImplementedException();
-    // TODO: insérer une instruction return ici
-}
-
-System::String^ CLMapEmployees::getN_E(void)
+System::String^ NS_Comp_Map_Employees::CLMapEmployees::getID_E(void)
 {
     throw gcnew System::NotImplementedException();
     // TODO: insérer une instruction return ici
 }
 
-System::String^ CLMapEmployees::getS_E(void)
+System::String^ NS_Comp_Map_Employees::CLMapEmployees::getN_E(void)
 {
     throw gcnew System::NotImplementedException();
     // TODO: insérer une instruction return ici
 }
 
-System::String^ CLMapEmployees::getHD_E(void)
+System::String^ NS_Comp_Map_Employees::CLMapEmployees::getS_E(void)
 {
     throw gcnew System::NotImplementedException();
     // TODO: insérer une instruction return ici
 }
 
-System::String^ CLMapEmployees::getN1_E(void)
+System::String^ NS_Comp_Map_Employees::CLMapEmployees::getHD_E(void)
 {
     throw gcnew System::NotImplementedException();
     // TODO: insérer une instruction return ici
 }
 
-System::String^ CLMapEmployees::getST_E(void)
+System::String^ NS_Comp_Map_Employees::CLMapEmployees::getN1_E(void)
+{
+    throw gcnew System::NotImplementedException();
+    // TODO: insérer une instruction return ici
+}
+
+System::String^ NS_Comp_Map_Employees::CLMapEmployees::getST_E(void)
 {
     throw gcnew System::NotImplementedException();
     // TODO: insérer une instruction return ici
