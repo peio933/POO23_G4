28b70a6d16c3bb2facdfc46785ae04f47b37dfe5
mebmal <150586282+mebmal@users.noreply.github.com>
2023-12-04 13:53:34 +0100

    Remplissage du CLMapStock.cpp (#4)
    Co-authored-by: Abdelmalik MEBARKI <abdelmalik.mebarki@viacesi.fr>

--- Projet_POO/CLMapStock.cpp
+++ Projet_POO/CLMapStock.cpp
@@ -2,123 +2,110 @@
 
 System::String^ CLMapStock::Select(void)
 {
-    throw gcnew System::NotImplementedException();
-    // TODO: insérer une instruction return ici
+    return "SELECT * FROM Article WHERE R_A = "+ getR_A() + "; ";
 }
 
 System::String^ CLMapStock::Insert(void)
 {
-    throw gcnew System::NotImplementedException();
-    // TODO: insérer une instruction return ici
+    return "INSERT INTO Article (R_A, N_A, QC_A, QS_A, RT_A, HT_A, TVA_A, D_A) VALUES(" + getR_A() + ", " + getN_A() + ", NULL, " + getQS_A() + ", " + getRT_A() + ", " + getHT_A() + ", " + getTVA_A() + ", " + getQS_A() + ";";
 }
 
 System::String^ CLMapStock::Delete(void)
 {
-    throw gcnew System::NotImplementedException();
-    // TODO: insérer une instruction return ici
+    return "DELETE FROM Article WHERE R_A = " + getR_A() + "AND N_A = " + getN_A() + "; ";
 }
 
 System::String^ CLMapStock::Update(void)
 {
-    throw gcnew System::NotImplementedException();
-    // TODO: insérer une instruction return ici
+    return "UPDATE Article SET QS_A = " + getQS_A() + ", HT_A = " + getHT_A() + ", RT_A = " + getRT_A() + ", TVA_A = " + getTVA_A() + ", D_A = " + getD_A() + " WHERE R_A = " + getR_A() + "; ";
 }
 
-void CLMapStock::setID_A(System::String^)
+void CLMapStock::setID_A(System::String^ ID_A)
 {
-    throw gcnew System::NotImplementedException();
+    this->ID_A = ID_A;
 }
 
-void CLMapStock::setR_A(System::String^)
+void CLMapStock::setR_A(System::String^ R_A)
 {
-    throw gcnew System::NotImplementedException();
+    this->R_A = R_A;
 }
 
-void CLMapStock::setN_A(System::String^)
+void CLMapStock::setN_A(System::String^ N_A)
 {
-    throw gcnew System::NotImplementedException();
+    this->N_A = N_A;
 }
 
-void CLMapStock::setQC_A(System::String^)
+void CLMapStock::setQC_A(System::String^ QC_A)
 {
-    throw gcnew System::NotImplementedException();
+    this->QC_A = QC_A;
 }
 
-void CLMapStock::setQS_A(System::String^)
+void CLMapStock::setQS_A(System::String^ QS_A)
 {
-    throw gcnew System::NotImplementedException();
+    this->QS_A = QS_A;
 }
 
-void CLMapStock::setHT_A(System::String^)
+void CLMapStock::setHT_A(System::String^ HT_A)
 {
-    throw gcnew System::NotImplementedException();
+    this->HT_A = HT_A;
 }
 
 void CLMapStock::setRT_A(System::String^)
 {
-    throw gcnew System::NotImplementedException();
+    this->RT_A = RT_A;
 }
 
 void CLMapStock::setTVA_A(System::String^)
 {
-    throw gcnew System::NotImplementedException();
+    this->TVA_A = TVA_A;
 }
 
 void CLMapStock::setD_A(System::String^)
 {
-    throw gcnew System::NotImplementedException();
+    this->D_A = D_A;
 }
 
 System::String^ CLMapStock::getID_A(void)
 {
-    throw gcnew System::NotImplementedException();
-    // TODO: insérer une instruction return ici
+    return this->ID_A;
 }
 
 System::String^ CLMapStock::getR_A(void)
 {
-    throw gcnew System::NotImplementedException();
-    // TODO: insérer une instruction return ici
+    return this->R_A;
 }
 
 System::String^ CLMapStock::getN_A(void)
 {
-    throw gcnew System::NotImplementedException();
-    // TODO: insérer une instruction return ici
+    return this->N_A;
 }
 
 System::String^ CLMapStock::getQC_A(void)
 {
-    throw gcnew System::NotImplementedException();
-    // TODO: insérer une instruction return ici
+    return this->QC_A;
 }
 
 System::String^ CLMapStock::getQS_A(void)
 {
-    throw gcnew System::NotImplementedException();
-    // TODO: insérer une instruction return ici
+    return this->QS_A;
 }
 
 System::String^ CLMapStock::getHT_A(void)
 {
-    throw gcnew System::NotImplementedException();
-    // TODO: insérer une instruction return ici
+    return this->R_A;
 }
 
 System::String^ CLMapStock::getRT_A(void)
 {
-    throw gcnew System::NotImplementedException();
-    // TODO: insérer une instruction return ici
+    return this->RT_A;
 }
 
 System::String^ CLMapStock::getTVA_A(void)
 {
-    throw gcnew System::NotImplementedException();
-    // TODO: insérer une instruction return ici
+    return this->TVA_A;
 }
 
 System::String^ CLMapStock::getD_A(void)
 {
-    throw gcnew System::NotImplementedException();
-    // TODO: insérer une instruction return ici
+    return this->D_A;
 }
