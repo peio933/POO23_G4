66fea7a0e5e167325c982cdf73b1d365238105e7
Peio Rigaud <peio.rigaud@viacesi.fr>
2023-12-05 15:57:35 +0100

    creation des boutons, champs texte, etc...

--- Projet_POO/PersonForm.h
+++ Projet_POO/PersonForm.h
@@ -35,6 +35,45 @@ namespace ProjetPOO {
 			}
 		}
 	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::DataGridView^ dataGridView1;
+	private: System::Windows::Forms::Button^ button2;
+	private: System::Windows::Forms::Button^ button3;
+	private: System::Windows::Forms::Button^ button4;
+	private: System::Windows::Forms::Button^ button5;
+	private: System::Windows::Forms::Button^ button6;
+	private: System::Windows::Forms::GroupBox^ groupBox1;
+	private: System::Windows::Forms::TextBox^ textBox1;
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::TextBox^ textBox2;
+	private: System::Windows::Forms::Label^ label3;
+
+	private: System::Windows::Forms::CheckBox^ checkBox1;
+	private: System::Windows::Forms::Label^ label4;
+	private: System::Windows::Forms::TextBox^ textBox3;
+	private: System::Windows::Forms::Label^ label5;
+	private: System::Windows::Forms::TextBox^ textBox4;
+	private: System::Windows::Forms::Label^ label6;
+	private: System::Windows::Forms::TextBox^ textBox5;
+	private: System::Windows::Forms::Label^ label7;
+
+	private: System::Windows::Forms::ListBox^ listBox1;
+	private: System::Windows::Forms::TextBox^ textBox6;
+	private: System::Windows::Forms::DateTimePicker^ dateTimePicker2;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 	protected:
 
 	protected:
@@ -77,6 +116,31 @@ namespace ProjetPOO {
 		{
 			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(PersonForm::typeid));
 			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->dataGridView1 = (gcnew System::Windows::Forms::DataGridView());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->button3 = (gcnew System::Windows::Forms::Button());
+			this->button4 = (gcnew System::Windows::Forms::Button());
+			this->button5 = (gcnew System::Windows::Forms::Button());
+			this->button6 = (gcnew System::Windows::Forms::Button());
+			this->groupBox1 = (gcnew System::Windows::Forms::GroupBox());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->checkBox1 = (gcnew System::Windows::Forms::CheckBox());
+			this->textBox3 = (gcnew System::Windows::Forms::TextBox());
+			this->label4 = (gcnew System::Windows::Forms::Label());
+			this->label5 = (gcnew System::Windows::Forms::Label());
+			this->textBox4 = (gcnew System::Windows::Forms::TextBox());
+			this->label6 = (gcnew System::Windows::Forms::Label());
+			this->textBox5 = (gcnew System::Windows::Forms::TextBox());
+			this->label7 = (gcnew System::Windows::Forms::Label());
+			this->textBox6 = (gcnew System::Windows::Forms::TextBox());
+			this->listBox1 = (gcnew System::Windows::Forms::ListBox());
+			this->dateTimePicker2 = (gcnew System::Windows::Forms::DateTimePicker());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->BeginInit();
+			this->groupBox1->SuspendLayout();
 			this->SuspendLayout();
 			// 
 			// button1
@@ -89,17 +153,246 @@ namespace ProjetPOO {
 			this->button1->UseVisualStyleBackColor = true;
 			this->button1->Click += gcnew System::EventHandler(this, &PersonForm::button1_Click);
 			// 
+			// dataGridView1
+			// 
+			this->dataGridView1->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
+			this->dataGridView1->Location = System::Drawing::Point(12, 12);
+			this->dataGridView1->Name = L"dataGridView1";
+			this->dataGridView1->Size = System::Drawing::Size(601, 275);
+			this->dataGridView1->TabIndex = 1;
+			// 
+			// button2
+			// 
+			this->button2->Cursor = System::Windows::Forms::Cursors::Hand;
+			this->button2->Location = System::Drawing::Point(12, 293);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(601, 57);
+			this->button2->TabIndex = 2;
+			this->button2->Text = L"LOAD";
+			this->button2->UseVisualStyleBackColor = true;
+			// 
+			// button3
+			// 
+			this->button3->Cursor = System::Windows::Forms::Cursors::Hand;
+			this->button3->Location = System::Drawing::Point(328, 418);
+			this->button3->Name = L"button3";
+			this->button3->Size = System::Drawing::Size(285, 57);
+			this->button3->TabIndex = 3;
+			this->button3->Text = L"DELETE";
+			this->button3->UseVisualStyleBackColor = true;
+			// 
+			// button4
+			// 
+			this->button4->Cursor = System::Windows::Forms::Cursors::Hand;
+			this->button4->Location = System::Drawing::Point(12, 356);
+			this->button4->Name = L"button4";
+			this->button4->Size = System::Drawing::Size(280, 57);
+			this->button4->TabIndex = 4;
+			this->button4->Text = L"SHOW";
+			this->button4->UseVisualStyleBackColor = true;
+			// 
+			// button5
+			// 
+			this->button5->Cursor = System::Windows::Forms::Cursors::Hand;
+			this->button5->Location = System::Drawing::Point(12, 418);
+			this->button5->Name = L"button5";
+			this->button5->Size = System::Drawing::Size(280, 57);
+			this->button5->TabIndex = 5;
+			this->button5->Text = L"UPDATE";
+			this->button5->UseVisualStyleBackColor = true;
+			// 
+			// button6
+			// 
+			this->button6->Cursor = System::Windows::Forms::Cursors::Hand;
+			this->button6->Location = System::Drawing::Point(328, 356);
+			this->button6->Name = L"button6";
+			this->button6->Size = System::Drawing::Size(285, 57);
+			this->button6->TabIndex = 6;
+			this->button6->Text = L"INSERT";
+			this->button6->UseVisualStyleBackColor = true;
+			// 
+			// groupBox1
+			// 
+			this->groupBox1->Controls->Add(this->listBox1);
+			this->groupBox1->Controls->Add(this->label7);
+			this->groupBox1->Controls->Add(this->textBox6);
+			this->groupBox1->Controls->Add(this->label6);
+			this->groupBox1->Controls->Add(this->textBox5);
+			this->groupBox1->Controls->Add(this->label5);
+			this->groupBox1->Controls->Add(this->textBox4);
+			this->groupBox1->Controls->Add(this->label4);
+			this->groupBox1->Controls->Add(this->textBox3);
+			this->groupBox1->Controls->Add(this->checkBox1);
+			this->groupBox1->Controls->Add(this->label3);
+			this->groupBox1->Controls->Add(this->dateTimePicker2);
+			this->groupBox1->Controls->Add(this->label2);
+			this->groupBox1->Controls->Add(this->textBox2);
+			this->groupBox1->Controls->Add(this->label1);
+			this->groupBox1->Controls->Add(this->textBox1);
+			this->groupBox1->Location = System::Drawing::Point(668, 12);
+			this->groupBox1->Name = L"groupBox1";
+			this->groupBox1->Size = System::Drawing::Size(318, 463);
+			this->groupBox1->TabIndex = 7;
+			this->groupBox1->TabStop = false;
+			this->groupBox1->Text = L"Informations";
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(71, 27);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(200, 20);
+			this->textBox1->TabIndex = 0;
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(15, 30);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(35, 13);
+			this->label1->TabIndex = 1;
+			this->label1->Text = L"Name";
+			this->label1->Click += gcnew System::EventHandler(this, &PersonForm::label1_Click);
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Location = System::Drawing::Point(15, 78);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(49, 13);
+			this->label2->TabIndex = 3;
+			this->label2->Text = L"Surname";
+			this->label2->Click += gcnew System::EventHandler(this, &PersonForm::label2_Click);
+			// 
+			// textBox2
+			// 
+			this->textBox2->Location = System::Drawing::Point(71, 75);
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->Size = System::Drawing::Size(200, 20);
+			this->textBox2->TabIndex = 2;
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Location = System::Drawing::Point(15, 127);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(64, 13);
+			this->label3->TabIndex = 5;
+			this->label3->Text = L"Date of Hire";
+			this->label3->Click += gcnew System::EventHandler(this, &PersonForm::label3_Click);
+			// 
+			// checkBox1
+			// 
+			this->checkBox1->AutoSize = true;
+			this->checkBox1->Location = System::Drawing::Point(204, 406);
+			this->checkBox1->Name = L"checkBox1";
+			this->checkBox1->Size = System::Drawing::Size(97, 17);
+			this->checkBox1->TabIndex = 6;
+			this->checkBox1->Text = L"Responsable \?";
+			this->checkBox1->UseVisualStyleBackColor = true;
+			// 
+			// textBox3
+			// 
+			this->textBox3->Location = System::Drawing::Point(101, 170);
+			this->textBox3->Name = L"textBox3";
+			this->textBox3->Size = System::Drawing::Size(98, 20);
+			this->textBox3->TabIndex = 7;
+			// 
+			// label4
+			// 
+			this->label4->AutoSize = true;
+			this->label4->Location = System::Drawing::Point(15, 173);
+			this->label4->Name = L"label4";
+			this->label4->Size = System::Drawing::Size(73, 13);
+			this->label4->TabIndex = 8;
+			this->label4->Text = L"Street number";
+			// 
+			// label5
+			// 
+			this->label5->AutoSize = true;
+			this->label5->Location = System::Drawing::Point(15, 218);
+			this->label5->Name = L"label5";
+			this->label5->Size = System::Drawing::Size(66, 13);
+			this->label5->TabIndex = 10;
+			this->label5->Text = L"Street Name";
+			this->label5->Click += gcnew System::EventHandler(this, &PersonForm::label5_Click);
+			// 
+			// textBox4
+			// 
+			this->textBox4->Location = System::Drawing::Point(101, 215);
+			this->textBox4->Name = L"textBox4";
+			this->textBox4->Size = System::Drawing::Size(200, 20);
+			this->textBox4->TabIndex = 9;
+			// 
+			// label6
+			// 
+			this->label6->AutoSize = true;
+			this->label6->Location = System::Drawing::Point(15, 265);
+			this->label6->Name = L"label6";
+			this->label6->Size = System::Drawing::Size(24, 13);
+			this->label6->TabIndex = 12;
+			this->label6->Text = L"City";
+			// 
+			// textBox5
+			// 
+			this->textBox5->Location = System::Drawing::Point(71, 262);
+			this->textBox5->Name = L"textBox5";
+			this->textBox5->Size = System::Drawing::Size(200, 20);
+			this->textBox5->TabIndex = 11;
+			this->textBox5->TextChanged += gcnew System::EventHandler(this, &PersonForm::textBox5_TextChanged);
+			// 
+			// label7
+			// 
+			this->label7->AutoSize = true;
+			this->label7->Location = System::Drawing::Point(15, 312);
+			this->label7->Name = L"label7";
+			this->label7->Size = System::Drawing::Size(63, 13);
+			this->label7->TabIndex = 14;
+			this->label7->Text = L"Postal code";
+			// 
+			// textBox6
+			// 
+			this->textBox6->Cursor = System::Windows::Forms::Cursors::IBeam;
+			this->textBox6->Location = System::Drawing::Point(90, 309);
+			this->textBox6->Name = L"textBox6";
+			this->textBox6->Size = System::Drawing::Size(200, 20);
+			this->textBox6->TabIndex = 13;
+			// 
+			// listBox1
+			// 
+			this->listBox1->FormattingEnabled = true;
+			this->listBox1->Location = System::Drawing::Point(18, 355);
+			this->listBox1->Name = L"listBox1";
+			this->listBox1->Size = System::Drawing::Size(200, 17);
+			this->listBox1->TabIndex = 15;
+			// 
+			// dateTimePicker2
+			// 
+			this->dateTimePicker2->Location = System::Drawing::Point(101, 122);
+			this->dateTimePicker2->Name = L"dateTimePicker2";
+			this->dateTimePicker2->Size = System::Drawing::Size(200, 20);
+			this->dateTimePicker2->TabIndex = 4;
+			// 
 			// PersonForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->BackColor = System::Drawing::SystemColors::ActiveCaption;
 			this->ClientSize = System::Drawing::Size(998, 516);
+			this->Controls->Add(this->groupBox1);
+			this->Controls->Add(this->button6);
+			this->Controls->Add(this->button5);
+			this->Controls->Add(this->button4);
+			this->Controls->Add(this->button3);
+			this->Controls->Add(this->button2);
+			this->Controls->Add(this->dataGridView1);
 			this->Controls->Add(this->button1);
 			this->Icon = (cli::safe_cast<System::Drawing::Icon^>(resources->GetObject(L"$this.Icon")));
 			this->Name = L"PersonForm";
 			this->Text = L"Employees Management";
 			this->Load += gcnew System::EventHandler(this, &PersonForm::Person_Management_Load);
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->EndInit();
+			this->groupBox1->ResumeLayout(false);
+			this->groupBox1->PerformLayout();
 			this->ResumeLayout(false);
 
 		}
@@ -119,6 +412,18 @@ private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e
 	this->Hide();
 	this->Close();
 
+}
+private: System::Void label1_Click(System::Object^ sender, System::EventArgs^ e) {
+}
+private: System::Void label2_Click(System::Object^ sender, System::EventArgs^ e) {
+}
+private: System::Void label3_Click(System::Object^ sender, System::EventArgs^ e) {
+}
+private: System::Void listView1_SelectedIndexChanged(System::Object^ sender, System::EventArgs^ e) {
+}
+private: System::Void label5_Click(System::Object^ sender, System::EventArgs^ e) {
+}
+private: System::Void textBox5_TextChanged(System::Object^ sender, System::EventArgs^ e) {
 }
 };
 }
