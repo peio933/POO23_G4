fdf57176a8d9fb8b97b2adaea4e56860e34a3fb2
Robin Noiret <119452365+RobinNoiret@users.noreply.github.com>
2023-12-08 11:09:04 +0100

    Implémentation de la simulation (statique) (#63)
    * Mise a jour de l'interface de simulation
    
    * Correctif affichage mal dimensionné
    
    * Backup | Ca compile
    
    * Implémentation de la simulation.
    La requête s'effectue en statique ! Et non dynamique comme attendu.
    
    Lors du passage en dynamique, une erreur est levée

--- Projet_POO/CLMapStats.cpp
+++ Projet_POO/CLMapStats.cpp
@@ -89,10 +89,12 @@ System::String^ NS_Comp_Map_Stats::CLMapStats::SumTTC_O_C(void)
 
 System::String^ NS_Comp_Map_Stats::CLMapStats::simulateStockValue(void)
 {
-	return "SELECT CAST(SUM(QC_A * HT_A) "
-		"AS DECIMAL(18, 2)) "
-		"AS Stock_commercial_value, "
-		"FROM Article;";
+	// Test statique
+	return " SELECT CAST(SUM(QC_A * HT_A * (1 + 0.80) * (1 + 0.15) * (1 - 0.03)) AS DECIMAL(18, 2))  AS Stock_commercial_value FROM Article; ";
+
+	//return "SELECT CAST(SUM(QC_A * HT_A * (1 + " + getTVA() + ") * (1 + " + getMargeCommerciale() + ") * (1 - " + getDemarqueInconnue() + ")) AS DECIMAL(18, 2)) AS Stock_commercial_value FROM Article;";
+	//" SELECT CAST(SUM(QC_A * HT_A * (1 + " + getTVA() + ") * (1 + " + getMargeCommerciale() + ") * (1 - " + getReduction() + ") * (1 - " + getDemarqueInconnue() + ")) AS DECIMAL(18, 2)) AS Stock_commercial_value FROM Article;";
+
 }
 
 void NS_Comp_Map_Stats::CLMapStats::setTVA(System::String^ TVA)
@@ -104,6 +106,11 @@ void NS_Comp_Map_Stats::CLMapStats::setMargeCommerciale(System::String^ MargeCom
 {
 	this->MargeCommerciale = MargeCommerciale;
 }
+
+//void NS_Comp_Map_Stats::CLMapStats::setReduction(System::String^ Reduction)
+//{
+	//this->Reduction = Reduction;
+//}
 
 void NS_Comp_Map_Stats::CLMapStats::setDemarqueInconnue(System::String^ DemarqueInconnue)
 {
