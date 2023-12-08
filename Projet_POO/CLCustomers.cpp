8572ac71175d00e73e0f0b6b0c886bf24a16d08c
AxelR69 <150123491+AxelR69@users.noreply.github.com>
2023-12-08 09:54:32 +0100

    Ajout des adresses dans CLMapCustomers (#62)

--- Projet_POO/CLCustomers.cpp
+++ Projet_POO/CLCustomers.cpp
@@ -13,38 +13,44 @@ System::Data::DataSet^ NS_Comp_Cus::CLCustomers::loadCustomer(System::String^ da
 	return this->oCAD->getRows(sql, dataTableName);
 }
 
-System::Data::DataSet^ NS_Comp_Cus::CLCustomers::selectCustomer(System::String^ N_C, System::String^ dataTableName)
+System::Data::DataSet^ NS_Comp_Cus::CLCustomers::selectCustomer(System::String^ S_C, System::String^ dataTableName)
 {
 	System::String^ sql;
-	this->oMapCustomers->setN_C(N_C);
+	this->oMapCustomers->setS_C(S_C);
 	sql = this->oMapCustomers->Select();
-	return this->oCAD->getRows(this->oMapCustomers->Select(), dataTableName);
+	return this->oCAD->getRows(sql, dataTableName);
 }
 
 void NS_Comp_Cus::CLCustomers::recordCustomer(System::String^ N_C, System::String^ S_C, System::String^ BD_C, System::String^ SNB, System::String^ SN, System::String^ N_V, System::String^ PC)
 {
+	System::String^ sql;
 	this->oMapCustomers->setN_C(N_C);
 	this->oMapCustomers->setS_C(S_C);
 	this->oMapCustomers->setBD_C(BD_C);
-	this->oMapCustomers->oAdress->setSNB(SNB);
-	this->oMapCustomers->oAdress->setSN(SN);
-	this->oMapCustomers->oAdress->setN_V(N_V);
-	this->oMapCustomers->oAdress->setPC(PC);
-	this->oCAD->actionRows(this->oMapCustomers->Insert());
+	this->oMapCustomers->setSNB(SNB);
+	this->oMapCustomers->setSN(SN);
+	this->oMapCustomers->setN_V(N_V);
+	this->oMapCustomers->setPC(PC);
+	sql = this->oMapCustomers->Insert();
+	this->oCAD->actionRows(sql);
 }
 
-void NS_Comp_Cus::CLCustomers::modifyCustomer(System::String^ N_C, System::String^ S_C, System::String^ BD_C)
+void NS_Comp_Cus::CLCustomers::modifyCustomer(System::String^ S_C, System::String^ N_C, System::String^ BD_C)
 {
-	this->oMapCustomers->setN_C(N_C);
+	System::String^ sql;
 	this->oMapCustomers->setS_C(S_C);
+	this->oMapCustomers->setN_C(N_C);
 	this->oMapCustomers->setBD_C(BD_C);
-	this->oCAD->actionRows(this->oMapCustomers->Update());
+	sql = this->oMapCustomers->Update();
+	this->oCAD->actionRows(sql);
 }
 
-void NS_Comp_Cus::CLCustomers::deleteCustomer(System::String^ N_C, System::String^ S_C, System::String^ BD_C)
+void NS_Comp_Cus::CLCustomers::deleteCustomer(System::String^ S_C, System::String^ N_C, System::String^ BD_C)
 {
-	this->oMapCustomers->setN_C(N_C);
+	System::String^ sql;
 	this->oMapCustomers->setS_C(S_C);
+	this->oMapCustomers->setN_C(N_C);
 	this->oMapCustomers->setBD_C(BD_C);
-	this->oCAD->actionRows(this->oMapCustomers->Delete());
+	sql = this->oMapCustomers->Delete();
+	this->oCAD->actionRows(sql);
 }
--- Projet_POO/CLMapCustomers.h
+++ Projet_POO/CLMapCustomers.h
@@ -1,14 +1,23 @@
 #pragma once
-#include "CLAdress.h"
 #include "CLMap.h"
 namespace NS_Comp_Map_Cus {
 	ref class CLMapCustomers : public CLMap
 	{
 	private:
-		System::String^ ID_C;
-		System::String^ N_C;
 		System::String^ S_C;
+		System::String^ N_C;
 		System::String^ BD_C;
+
+
+		//adresse client
+		System::String^ ID_CY;
+		System::String^ N_V;
+		System::String^ ID_SN;
+		System::String^ SN;
+		System::String^ ID_SNB;
+		System::String^ SNB;
+		System::String^ ID_PC;
+		System::String^ PC;
 	public:
 		System::String^ SelectAll(void);
 		System::String^ Select(void) override;
@@ -16,14 +25,34 @@ namespace NS_Comp_Map_Cus {
 		System::String^ Delete(void) override;
 		System::String^ Update(void) override;
 
-		void setID_C(System::String^);
-		void setN_C(System::String^);
 		void setS_C(System::String^);
+		void setN_C(System::String^);
 		void setBD_C(System::String^);
 
-		System::String^ getID_C(void);
-		System::String^ getN_C(void);
+
+		//adresse client
+		void setID_CY(System::String^);
+		void setN_V(System::String^);
+		void setID_SN(System::String^);
+		void setSN(System::String^);
+		void setID_SNB(System::String^);
+		void setSNB(System::String^);
+		void setID_PC(System::String^);
+		void setPC(System::String^);
+
 		System::String^ getS_C(void);
+		System::String^ getN_C(void);
 		System::String^ getBD_C(void);
+
+		//adresse
+		System::String^ getID_CY(void);
+		System::String^ getN_V(void);
+		System::String^ getID_SN(void);
+		System::String^ getSN(void);
+		System::String^ getID_SNB(void);
+		System::String^ getSNB(void);
+		System::String^ getID_PC(void);
+		System::String^ getPC(void);
+
 	};
 }
