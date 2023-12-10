fe59cd96866b6b17334cdb8508af6b6a15544100
Picabea Peïo <116553253+peio933@users.noreply.github.com>
2023-12-10 20:16:06 +0100

    correction des requetes de delete + rajoute des commentaire. (#83)

--- Projet_POO/CLStats.cpp
+++ Projet_POO/CLStats.cpp
@@ -1,67 +1,90 @@
 #include "CLStats.h"
 
+// Constructeur de la classe CLStats
 NS_Comp_Stats::CLStats::CLStats(void)
 {
+    // Initialisation des membres de la classe
     this->oCAD = gcnew NS_Comp_Data::CLCAD();
     this->oMapStats = gcnew NS_Comp_Map_Stats::CLMapStats();
 }
 
+// Calcul de la moyenne du panier
 System::Data::DataSet^ NS_Comp_Stats::CLStats::averageCart(System::String^ dataTableName)
 {
     return this->oCAD->getRows(this->oMapStats->AverageTTC_O(), dataTableName);
 }
 
+// Calcul du chiffre d'affaires mensuel
 System::Data::DataSet^ NS_Comp_Stats::CLStats::monthSalesRevenue(System::String^ dataTableName, System::String^ Month, System::String^ Year)
 {
+    // Configuration du mois et de l'année dans l'objet oMapStats
     this->oMapStats->setMonthSalesRevenue(Month);
     this->oMapStats->setYearSalesRevenue(Year);
-    return this->oCAD->getRows(this->oMapStats->SumTTC_O(), dataTableName);
 
+    // Exécution de la requête SQL et retour des résultats
+    return this->oCAD->getRows(this->oMapStats->SumTTC_O(), dataTableName);
 }
 
+// Sélection des articles en dessous du seuil de vente
 System::Data::DataSet^ NS_Comp_Stats::CLStats::belowThreshold(System::String^ dataTableName)
 {
+    // Exécution de la requête SQL et retour des résultats
     return this->oCAD->getRows(this->oMapStats->SelectbelowThreshold(), dataTableName);
 }
 
+// Sélection des meilleurs vendeurs
 System::Data::DataSet^ NS_Comp_Stats::CLStats::bestSellers(System::String^ dataTableName)
 {
+    // Exécution de la requête SQL et retour des résultats
     return this->oCAD->getRows(this->oMapStats->SelectBestSellers(), dataTableName);
 }
 
+// Sélection des articles les moins vendus
 System::Data::DataSet^ NS_Comp_Stats::CLStats::lessSold(System::String^ dataTableName)
 {
+    // Exécution de la requête SQL et retour des résultats
     return this->oCAD->getRows(this->oMapStats->SelectLessSold(), dataTableName);
 }
 
+// Calcul de la valeur commerciale du stock
 System::Data::DataSet^ NS_Comp_Stats::CLStats::stockCommercialValue(System::String^ dataTableName)
 {
+    // Exécution de la requête SQL et retour des résultats
     return this->oCAD->getRows(this->oMapStats->QueryStockCommercialValue(), dataTableName);
 }
 
+// Calcul de la valeur d'achat du stock
 System::Data::DataSet^ NS_Comp_Stats::CLStats::stockPurchaseValue(System::String^ dataTableName)
 {
+    // Exécution de la requête SQL et retour des résultats
     return this->oCAD->getRows(this->oMapStats->QueryStockPurchaseValue(), dataTableName);
 }
 
+// Calcul du montant total des achats par client
 System::Data::DataSet^ NS_Comp_Stats::CLStats::customerTotalAmount(System::String^ dataTableName)
 {
+    // Exécution de la requête SQL et retour des résultats
     return this->oCAD->getRows(this->oMapStats->SumTTC_O_C(), dataTableName);
 }
+
 /*
+// Simulation de la valeur du stock (commenté pour le moment)
 System::Data::DataSet^ NS_Comp_Stats::CLStats::simulateStockValue(System::String^ dataTableName)
 {
     return this->oCAD->getRows(this->oMapStats->simulateStockValue(), dataTableName);
 }
 */
 
-
+// Simulation de la valeur du stock avec des paramètres spécifiques
 System::Data::DataSet^ NS_Comp_Stats::CLStats::simulateStockValue(System::String^ TVA, System::String^ MargeCommerciale, System::String^ ReductionCommerciale, System::String^ DemarqueInconnue, System::String^ dataTableName)
 {
+    // Configuration des paramètres dans l'objet oMapStats
     this->oMapStats->setTVA(TVA);
     this->oMapStats->setMargeCommerciale(MargeCommerciale);
     this->oMapStats->setReductionCommerciale(ReductionCommerciale);
     this->oMapStats->setDemarqueInconnue(DemarqueInconnue);
 
+    // Exécution de la requête SQL et retour des résultats
     return this->oCAD->getRows(this->oMapStats->simulateStockValue(), dataTableName);
 }
+
--- Projet_POO/CLStock.cpp
+++ Projet_POO/CLStock.cpp
@@ -1,28 +1,42 @@
 #include "CLStock.h"
 
+// Constructeur de la classe CLStock
 NS_Comp_Stock::CLStock::CLStock(void)
 {
+    // Initialisation des membres de la classe
     this->oCAD = gcnew NS_Comp_Data::CLCAD();
     this->oMapStock = gcnew NS_Comp_Map_Stock::CLMapStock();
 }
 
+// Chargement de tous les articles depuis la base de données
 System::Data::DataSet^ NS_Comp_Stock::CLStock::loadArticles(System::String^ dataTableName)
 {
+    // Construction de la requête SQL pour récupérer tous les articles
     System::String^ sql;
     sql = this->oMapStock->SelectAll();
+
+    // Exécution de la requête SQL et retour des résultats
     return this->oCAD->getRows(sql, dataTableName);
 }
 
+// Sélection d'un article spécifique en fonction de la référence
 System::Data::DataSet^ NS_Comp_Stock::CLStock::selectArticle(System::String^ R_A, System::String^ dataTableName)
 {
+    // Configuration de la référence dans l'objet oMapStock
     this->oMapStock->setR_A(R_A);
+
+    // Construction de la requête SQL pour la sélection de l'article
     System::String^ sql;
     sql = this->oMapStock->Select();
+
+    // Exécution de la requête SQL et retour des résultats
     return this->oCAD->getRows(sql, dataTableName);
 }
 
+// Insertion d'un nouvel article dans la base de données
 void NS_Comp_Stock::CLStock::insertArticle(System::String^ R_A, System::String^ N_A, System::String^ QS_A, System::String^ RT_A, System::String^ HT_A, System::String^ TVA_A, System::String^ D_A)
 {
+    // Configuration des attributs de l'article dans l'objet oMapStock
     this->oMapStock->setR_A(R_A);
     this->oMapStock->setN_A(N_A);
     this->oMapStock->setQS_A(QS_A);
@@ -30,23 +44,35 @@ void NS_Comp_Stock::CLStock::insertArticle(System::String^ R_A, System::String^ 
     this->oMapStock->setHT_A(HT_A);
     this->oMapStock->setTVA_A(TVA_A);
     this->oMapStock->setD_A(D_A);
+
+    // Exécution de l'action d'insertion dans la base de données
     this->oCAD->actionRows(this->oMapStock->Insert());
 }
 
+// Mise à jour des informations d'un article dans la base de données
 void NS_Comp_Stock::CLStock::updateArticle(System::String^ R_A, System::String^ QS_A, System::String^ RT_A, System::String^ HT_A, System::String^ TVA_A, System::String^ D_A)
 {
+    // Configuration des attributs modifiés de l'article dans l'objet oMapStock
     this->oMapStock->setQS_A(QS_A);
     this->oMapStock->setHT_A(HT_A);
     this->oMapStock->setRT_A(RT_A);
     this->oMapStock->setTVA_A(TVA_A);
     this->oMapStock->setD_A(D_A);
+
+    // Configuration de la référence dans l'objet oMapStock
     this->oMapStock->setR_A(R_A);
+
+    // Exécution de l'action de mise à jour dans la base de données
     this->oCAD->actionRows(this->oMapStock->Update());
 }
 
+// Suppression d'un article de la base de données
 System::Data::DataSet^ NS_Comp_Stock::CLStock::deleteArticle(System::String^ R_A, System::String^ N_A, System::String^ dataTableName)
 {
+    // Configuration des attributs de l'article à supprimer dans l'objet oMapStock
     this->oMapStock->setR_A(R_A);
     this->oMapStock->setN_A(N_A);
+
+    // Exécution de l'action de suppression dans la base de données
     return this->oCAD->getRows(this->oMapStock->Delete(), dataTableName);
 }
